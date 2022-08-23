8a68d49467fe5b0f82def3d99ff9e52790533043
ariannadepasquale <depasqualearianna@gmail.com>
2022-08-23 17:13:42 +0200

    First Milestone Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <MyTitle />
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyTitle from '@/components/MyTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyTitle
   }
 }
 </script>
---
+++ src/components/MyTitle.vue
@@ -0,0 +1,16 @@
+<template>
+<h3>La mia prima app con Vue CLI</h3>
+
+</template>
+
+<script>
+export default {
+    name: 'MyTitle',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
